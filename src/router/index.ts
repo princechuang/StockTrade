4136652daea737f382359ac07bcca92af4219190
TangChuang <princechuang@live.com>
2024-11-20 18:08:31 +0800

    修改获取用户信息接口

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import {
   RouteLocationNormalized,
   RouteRecordRaw,
 } from "vue-router";
-import { showToast } from "vant";
+import { showFailToast, showToast } from "vant";
 import { getUserInfoByToken } from "@/api/myApi";
 import { isPro, portalHost, appId, agentId,apiHost } from "@/utils/configs";
 import 'vant/es/toast/style';
@@ -80,13 +80,16 @@ router.beforeEach((to, from, next) => {
      sessionStorage.setItem("token", token as string);
       //通过code获取登录用户的UserId
       
-      getUserInfoByToken(token as string).then((res:any) => {
+      getUserInfoByToken(token as string)
+      .then((res:any) => {
         console.log(res);
         //缓存用户信息
         sessionStorage.setItem("userId",res.data.detail.userId);
         sessionStorage.setItem("userInfo",JSON.stringify(res.data.detail));
 
         next();
+      }).catch(err=>{
+        showFailToast("获取用户信息失败");
       });
     } else {
       //跳转到未授权页面
